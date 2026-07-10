cc0e2e9a395476de85b3ad004683c20edbdc3886
madsilx10 <115327876+madsilx10@users.noreply.github.com>
2026-07-11 01:46:25 +0700

    Add files via upload

--- faucet.js
+++ faucet.js
@@ -43,7 +43,8 @@ async function solveTurnstile() {
       '--no-sandbox',
       '--disable-setuid-sandbox',
       '--disable-dev-shm-usage',
-      '--disable-gpu'
+      '--disable-gpu',
+      '--single-process'
     ]
   });
 
